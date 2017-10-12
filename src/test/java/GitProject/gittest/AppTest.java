7e7e99b4724d264bf53fbb6aee6a87c4220f1d77
Enislav Mollov <eniii@mail.bg>
2017-10-12 10:00:55 +0300

    google

--- src/test/java/GitProject/gittest/AppTest.java
+++ src/test/java/GitProject/gittest/AppTest.java
@@ -19,29 +19,14 @@ public class AppTest
     @Test
 	public void testApp()
     {
-    	//String exePath = "/Users/enislavmollov/Downloads/chromedriver";
-		System.setProperty("webdriver.chrome.driver", "/Users/enislavmollov/Downloads/chromedriver");
+    	String exePath = "/Users/enislavmollov/Downloads/chromedriver";
+		System.setProperty("webdriver.chrome.driver", exePath);
 		WebDriver driver = new ChromeDriver();
 
-		driver.get("http://beta.capecloud.ch");
+		driver.get("http://google.com");
 
-		WebDriverWait wait = new WebDriverWait(driver, 15);
-
-		// Enter credentials
-
-		driver.findElement(By.id("username")).sendKeys("adminuser");
-
-		driver.findElement(By.id("password")).sendKeys("testing");
-
-		driver.findElement(By.id("sign-in")).click();
-
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("sms_code")));
-
-		driver.findElement(By.id("sms_code")).sendKeys("0");
-
-		driver.findElement(By.id("sign-in")).click();
-
-		wait.until(ExpectedConditions.urlToBe("http://beta.capecloud.ch/#/"));
-
+		System.out.println("Success");
+		
+		driver.quit();
     }
 }
