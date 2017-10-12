68efc0b20e70684312fbe7af7c71da806b4911f4
Enislav Mollov <eniii@mail.bg>
2017-10-12 09:43:50 +0300

    updated script

--- src/test/java/GitProject/gittest/AppTest.java
+++ src/test/java/GitProject/gittest/AppTest.java
@@ -1,6 +1,12 @@
 package GitProject.gittest;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.AssertJUnit;
 
 /**
@@ -23,6 +29,29 @@ public class AppTest
     @Test
 	public void testApp()
     {
-        AssertJUnit.assertTrue( true );
+    	String exePath = "/Users/enislavmollov/Downloads/chromedriver";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://beta.capecloud.ch");
+
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+
+		// Enter credentials
+
+		driver.findElement(By.id("username")).sendKeys("adminuser");
+
+		driver.findElement(By.id("password")).sendKeys("testing");
+
+		driver.findElement(By.id("sign-in")).click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("sms_code")));
+
+		driver.findElement(By.id("sms_code")).sendKeys("0");
+
+		driver.findElement(By.id("sign-in")).click();
+
+		wait.until(ExpectedConditions.urlToBe("http://beta.capecloud.ch/#/"));
+
     }
 }
