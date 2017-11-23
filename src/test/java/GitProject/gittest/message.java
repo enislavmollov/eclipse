3b1064b562e694f4c9eea36f8d26f8e0fc75c25d
Enislav Mollov <eniii@mail.bg>
2017-11-23 11:27:53 +0200

    тест

--- src/test/java/GitProject/gittest/message.java
+++ src/test/java/GitProject/gittest/message.java
@@ -15,8 +15,8 @@ public class message {
   @Test
   public void mess() throws InterruptedException {
 	  
-	 String exePath = "/Users/Shared/Jenkins/Home/chromedriver";
-		//String exePath = "C:\\Program Files (x86)\\Jenkins\\workspace\\chromedriver.exe";
+	 //String exePath = "/Users/Shared/Jenkins/Home/chromedriver";
+		String exePath = "C:\\Program Files (x86)\\Jenkins\\workspace\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", exePath);
 		WebDriver driver = new ChromeDriver();
 	  
