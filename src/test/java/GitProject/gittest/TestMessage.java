a8b4eb057a7342612a84d0cf79fe397e0c364ddf
Enislav Mollov <eniii@mail.bg>
2017-11-23 11:43:04 +0200

    2 теста

--- src/test/java/GitProject/gittest/TestMessage.java
+++ src/test/java/GitProject/gittest/TestMessage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class message {
+public class TestMessage {
   @Test
   public void mess() throws InterruptedException {
 	  
